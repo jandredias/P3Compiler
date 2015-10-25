8cdabe998c84089845933843a461a0aee5743d79
Andre Dias <jdias.email@gmail.com>
2015-10-25 15:52:49 +0000

    Initial commit!

---
+++ libcdk10-201502141815/emitters/postfix_ix86_emitter.h
@@ -0,0 +1,381 @@
+// $Id: postfix_ix86_emitter.h,v 1.2 2014/02/25 21:43:13 david Exp $ -*- c++ -*-
+#ifndef __CDK9_GENERATOR_IX86_H__
+#define __CDK9_GENERATOR_IX86_H__
+
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cctype>
+#include <cdk/emitters/basic_postfix_emitter.h>
+
+namespace cdk {
+
+  /** Class postfix_ix86_emitter: emitter for yasm/nasm code.  */
+  class postfix_ix86_emitter: public basic_postfix_emitter {
+
+    inline void debug(const std::string &s) {
+      if (basic_postfix_emitter::debug()) {
+        os() << "; " << s << std::endl;
+      }
+    }
+
+    template<typename Type>
+    inline void debug(const std::string &s, const Type &value) {
+      if (basic_postfix_emitter::debug()) {
+        os() << "; " << s << " " << value << std::endl;
+      }
+    }
+
+    //----------------------------------------------------------------------
+
+  public:
+    inline postfix_ix86_emitter(std::shared_ptr<compiler> &compiler) :
+        basic_postfix_emitter(compiler) {
+      // Ensure that double literals are generated
+      // in accordance with NASM rules.
+      os() << std::setiosflags(std::ios::showpoint);
+    }
+
+    //----------------------------------------------------------------------
+
+  private:
+    template<typename T> void __cmd1(std::string mnemonic, T arg) {
+      os() << "\t" << mnemonic << "\t" << arg << std::endl;
+    }
+    template<typename T1, typename T2> void __cmd2(std::string mnemonic, T1 arg1, T2 arg2) {
+      os() << "\t" << mnemonic << "\t" << arg1 << ", " << arg2 << std::endl;
+    }
+    template<typename T> std::string _byte(T what) {
+      std::ostringstream oss;
+      oss << "byte " << what;
+      return oss.str();
+    }
+    template<typename T> std::string _word(T what) {
+      std::ostringstream oss;
+      oss << "word " << what;
+      return oss.str();
+    }
+    template<typename T> std::string _dword(T what) {
+      std::ostringstream oss;
+      oss << "dword " << what;
+      return oss.str();
+    }
+    template<typename T> std::string _qword(T what) {
+      std::ostringstream oss;
+      oss << "qword " << what;
+      return oss.str();
+    }
+    template<typename T> std::string _lbladdr(T what) {
+      std::ostringstream oss;
+      oss << "$" << what;
+      return oss.str();
+    }
+    template<typename T> std::string _deref(T what) {
+      std::ostringstream oss;
+      oss << "[" << what << "]";
+      return oss.str();
+    }
+    template<typename T> std::string _offset(T what, int offset) {
+      std::ostringstream oss;
+      if (offset < 0)
+        oss << what << "-" << -offset;
+      else if (offset > 0)
+        oss << what << "+" << offset;
+      else
+        oss << what;
+      return oss.str();
+    }
+    template<typename T> std::string _deref(T what, int offset) {
+      return _deref(_offset(what, offset));
+    }
+    void _pop(std::string what) {
+      __cmd1("pop", what);
+    }
+    void _push(std::string what) {
+      __cmd1("push", what);
+    }
+    void _mov(std::string a, std::string b) {
+      __cmd2("mov", a, b);
+    }
+
+    //!
+    //! Arithmetic operations
+    //!
+    template<typename T> void _add(std::string a, T b) {
+      __cmd2("add", a, b);
+    }
+    template<typename T> void _sub(std::string a, T b) {
+      __cmd2("sub", a, b);
+    }
+    void _neg(std::string a) {
+      __cmd1("neg", a);
+    }
+    void _imul(std::string a, std::string b) {
+      __cmd2("imul", a, b);
+    }
+
+    /* Comparison */
+    void _cmp(std::string a, std::string b) {
+      __cmd2("cmp", a, b);
+    }
+
+    /* Logical operations */
+    void _xor(std::string a, std::string b) {
+      __cmd2("xor", a, b);
+    }
+    void _and(std::string a, std::string b) {
+      __cmd2("and", a, b);
+    }
+    void _or(std::string a, std::string b) {
+      __cmd2("or", a, b);
+    }
+    void _not(std::string a) {
+      __cmd1("not", a);
+    }
+
+    /* Rotation and shift operations */
+    void _rol(std::string a, std::string b) {
+      __cmd2("rol", a, b);
+    }
+    void _ror(std::string a, std::string b) {
+      __cmd2("ror", a, b);
+    }
+    void _sal(std::string a, std::string b) {
+      __cmd2("sal", a, b);
+    }
+    void _sar(std::string a, std::string b) {
+      __cmd2("sar", a, b);
+    }
+    void _shr(std::string a, std::string b) {
+      __cmd2("shr", a, b);
+    }
+
+    /* Calls, jumps, etc. */
+    void _call(std::string what) {
+      __cmd1("call", what);
+    }
+    void _jmp(std::string what) {
+      __cmd1("jmp", what);
+    }
+
+    /* Segments */
+    void _segment(std::string what) {
+      os() << "segment\t" << what << "\n";
+    }
+
+    /* Floating point */
+    void _fild(std::string what) {
+      __cmd1("fild", what);
+    }
+    void _fistp(std::string what) {
+      __cmd1("fistp", what);
+    }
+    void _fld(std::string what) {
+      __cmd1("fld", what);
+    }
+    void _fstp(std::string what) {
+      __cmd1("fstp", what);
+    }
+
+    //----------------------------------------------------------------------
+
+    // Implementation of the postfix interface
+
+  public:
+
+       void ORIG(int pos) {
+         os() << "ORIG " << pos << std::endl;
+       }
+       void EQU(std::string label, int value) {
+         os() << label << " EQU " << value << std::endl;
+       }
+       void WORD(std::string label, int value) {
+         os() << label << " WORD " << value << std::endl;}
+       void STR(std::string label, std::string value) {
+         os() << label << " STR" << value << std::endl;}
+       void TAB(std::string label, int value) {
+         os() << label << " TAB " << value << std::endl;}
+       void ADD() {
+         os() << "POP R1" << std::endl;
+         os() << "ADD M[SP + 1], R1" << std::endl;
+       }
+       void ADDC() {
+         os() << "POP R1" << std::endl;
+         os() << "ADDC M[SP + 1], R1" << std::endl;
+       }
+       void AND() {
+         os() << "POP R1" << std::endl;
+         os() << "AND M[SP + 1], R1" << std::endl;
+       }
+       void BRCOND(std::string cond, std::string value) {
+        if(cond == std::to_string('C') || cond == std::string("NC") ||
+            cond == std::to_string('N') || cond == std::string("NN") ||
+            cond == std::to_string('O') || cond == std::string("NO") ||
+            cond == std::to_string('Z') || cond == std::string("NZ") ||
+            cond == std::to_string('I') || cond == std::string("NI") ||
+            cond == std::to_string('P') || cond == std::string("NP")){
+          os() << "BR." << cond << " " << value << std::endl;
+        }
+       }
+       void BR(std::string label) {
+         os() << "BR " << label << std::endl;
+       }
+       void CALL(std::string value) {
+         os() << "CALL " << value << std::endl;
+       }
+       void CALLCOND(std::string cond, std::string value) {
+         if(cond == std::to_string('C') || cond == std::string("NC") ||
+            cond == std::to_string('N') || cond == std::string("NN") ||
+            cond == std::to_string('O') || cond == std::string("NO") ||
+            cond == std::to_string('Z') || cond == std::string("NZ") ||
+            cond == std::to_string('I') || cond == std::string("NI") ||
+            cond == std::to_string('P') || cond == std::string("NP")){
+          os() << "CALL." << cond << " " << value << std::endl;
+        }
+       }
+       void CLC() {
+         os() << "CLC" << std::endl;
+       }
+       void CMC() {
+         os() << "CMC" << std::endl;
+       }
+       void CMP() {
+         os() << "CMP M[SP + 1], R1" << std::endl;
+       }
+       void COM() {
+         os() << "COM M[SP + 1]" << std::endl;
+       }
+       void DEC() {
+         os() << "DEC M[SP + 1]" << std::endl;
+       }
+       void DIV() {
+         os() << "POP R1" << std::endl;
+         os() << "POP R2" << std::endl;
+         os() << "DIV R1, R2" << std::endl;
+         os() << "PUSH R1" << std::endl;
+         os() << "PUSH R2" << std::endl;
+       }
+       void DSI() {
+         os() << "DSI" << std::endl;
+       }
+       void ENI() {
+         os() << "ENI" << std::endl;
+       }
+       void INC() {
+         os() << "INC M[SP + 1]" << std::endl;
+       }
+       void INT(int value) {
+         os() << "INT " << value << std::endl;
+       }
+       void JMP(std::string value) {
+         os() << "JMP " << value << std::endl;
+       }
+       void JMPCOND(std::string cond, std::string value) {
+         if(cond == std::to_string('C') || cond == std::string("NC") ||
+            cond == std::to_string('N') || cond == std::string("NN") ||
+            cond == std::to_string('O') || cond == std::string("NO") ||
+            cond == std::to_string('Z') || cond == std::string("NZ") ||
+            cond == std::to_string('I') || cond == std::string("NI") ||
+            cond == std::to_string('P') || cond == std::string("NP")){
+          os() << "JMP." << cond << " " << value << std::endl;
+        }
+       }
+       void MOV() {
+         //TODO
+       }
+       void MUL() {
+         os() << "POP R1" << std::endl;
+         os() << "MUL M[SP + 1], R1" << std::endl;
+         os() << "PUSH R1" << std::endl;
+       }
+       void MVBH() {
+         os() << "POP R1" << std::endl;
+         os() << "MVBH M[SP + 1], R1" << std::endl;
+       }
+       void MVBL() {
+         os() << "POP R1" << std::endl;
+         os() << "MVBL M[SP + 1], R1" << std::endl;
+       }
+       void NEG() {
+         os() << "NEG M[SP + 1]" << std::endl;
+       }
+       void NOP() {
+         os() << "NOP" << std::endl;
+       }
+       void OR() {
+         os() << "POP R1" << std::endl;
+         os() << "OR M[SP + 1], R1" << std::endl;
+       }
+       void POP() {
+         os() << "POP R1" << std::endl;
+       }
+       void PUSH(int value) {
+         os() << "PUSH " << value << std::endl;
+       }
+       void RET() {
+         os() << "RET" << std::endl;
+       }
+       void RETN(int value) {
+         os() << "RET " << value << std::endl;
+       }
+       void ROL(int value) {
+         os() << "ROL M[SP + 1], " << value << std::endl;
+       }
+       void ROLC(int value) {
+         os() << "ROLC M[SP + 1], " << value << std::endl;
+       }
+       void ROR(int value) {
+         os() << "ROR M[SP + 1], " << value << std::endl;
+       }
+       void RORC(int value) {
+         os() << "RORC M[SP + 1], " << value << std::endl;
+       }
+       void RTI() {
+         os() << "RTI" << std::endl;
+       }
+       void SHL(int value) {
+         os() << "SHL M[SP + 1], " << value << std::endl;
+       }
+       void SHLA(int value) {
+         os() << "SHLA M[SP + 1], " << value << std::endl;
+       }
+       void SHR(int value) {
+         os() << "SHR M[SP + 1], " << value << std::endl;
+       }
+       void SHRA(int value) {
+         os() << "SHRA M[SP + 1], " << value << std::endl;
+       }
+       void STC() {
+         os() << "STC" << std::endl;
+       }
+       void SUB() {
+         os() << "POP R1" << std::endl;
+         os() << "SUB M[SP + 1], R1" << std::endl;
+       }
+       void SUBB() {
+         os() << "POP R1" << std::endl;
+         os() << "SUBB M[SP + 1], R1" << std::endl;
+       }
+       void TEST() {
+         //TODO
+       }
+       void XCH() {
+         os() << "POP R1" << std::endl;
+         os() << "POP R2" << std::endl;
+         os() << "PUSH R1" << std::endl;
+         os() << "PUSH R2" << std::endl;
+       }
+       void XOR() {
+         os() << "POP R1" << std::endl;
+         os() << "XOR M[SP + 1], R1" << std::endl;
+       }
+
+       void LABEL(std::string label){
+         os() << label << ":";
+       }
+
+  };
+
+} // namespace cdk
+
+#endif
---
+++ project/pwn_parser.y
@@ -0,0 +1,166 @@
+%{
+// $Id: pwn_parser.y,v 1.29 2015/05/19 16:38:00 ist175741 Exp $
+//-- don't change *any* of these: if you do, you'll break the compiler.
+#include <cdk/compiler.h>
+#include "ast/all.h"
+#define LINE       compiler->scanner()->lineno()
+#define yylex()    compiler->scanner()->scan()
+#define yyerror(s) compiler->scanner()->error(s)
+#define YYPARSE_PARAM_TYPE std::shared_ptr<cdk::compiler>
+#define YYPARSE_PARAM      compiler
+//-- don't change *any* of these --- END!
+%}
+
+%union {
+  int                   i;          /* integer value */
+  double                d;	        /* double value */
+  std::string          *s;	        /* symbol name or string literal */
+  cdk::basic_node	     *node;	      /* node pointer */
+  cdk::sequence_node	 *sequence;
+  cdk::expression_node *expression; /* expression nodes */
+  pwn::lvalue_node     *lvalue;
+  pwn::block_node      *block;
+  basic_type           *t;	        /*basic_type*/
+};
+
+%token <i> tINTEGER
+%token <d> tDOUBLE
+%token <s> tID tSTRING
+%token tREPEAT tIF tPRINT tREAD tBEGIN tEND tNEXT tSTOP tLOCAL tIMPORT tRETURN tNOOB
+%token tTYPE_INT tPLN tEND tVOID tINT tSTRING
+
+%nonassoc tIFX
+%nonassoc tELSE
+%nonassoc '(' ')' '~' '[' ']'
+
+%right '='
+%left '|'
+%left '&'
+%left tGE tLE tEQ tNE '>' '<' '{'
+%left '+' '-'
+%left '*' '/' '%'
+%nonassoc tUNARY
+
+
+%type <node> stmt dec v_dec f_dec f_def
+%type <sequence> decs   vars arg_call stmts
+%type <expression> expr literal f_call init
+%type <i>
+%type <lvalue> lval
+%type <block> body
+%type <t> type
+%type <s> strlist
+
+%{
+//-- The rules below will be included in yyparse, the main parsing function.
+%}
+
+%%
+file    : decs    { compiler->ast($1); }
+        ;
+
+decs    : dec      { $$ = new cdk::sequence_node(LINE, $1); }
+        | decs dec { $$ = new cdk::sequence_node(LINE, $2, $1); }
+        ;
+
+dec     : v_dec tEND { $$ = $1; }
+        | f_dec tEND { $$ = $1; }
+        | f_def      { $$ = $1; }
+        ;
+
+v_dec   : type tID '=' literal { $$ = new pwn::var_dec_node(LINE, $1, $2, $4); }
+        | type tID             { $$ = new pwn::var_dec_node(LINE, $1, $2); }
+        | '*'  tID             { $$ = new pwn::var_dec_node(LINE, new basic_type(4, basic_type::TYPE_POINTER), $2); }
+        | '*'  tID '=' tNOOB   { $$ = new pwn::var_dec_node(LINE, new basic_type(4, basic_type::TYPE_POINTER), $2, new pwn::noob_node(LINE)); }
+        ;
+
+f_dec   : type tID '(' ')' { $$ = new pwn::function_dec_node(LINE, $2, NULL, $1, true); }
+        | type tID '(' vars ')' { $$ = new pwn::function_dec_node(LINE, $2, $4, $1, true); }
+        | tVOID tID '(' vars ')' { $$ = new pwn::function_dec_node(LINE, $2, $4, new basic_type(0,basic_type::TYPE_VOID), true); }
+        | tVOID tID '(' ')' { $$ = new pwn::function_dec_node(LINE, $2, NULL, new basic_type(0,basic_type::TYPE_VOID), true); }
+        | '*' tID '(' vars ')' { $$ = new pwn::function_dec_node(LINE, $2, $4, new basic_type(4,basic_type::TYPE_POINTER), true); }
+        | '*' tID '(' ')' { $$ = new pwn::function_dec_node(LINE, $2, NULL, new basic_type(4,basic_type::TYPE_POINTER), true); }
+        ;
+
+f_def   : type tID '(' vars ')' body  { $$ = new pwn::function_def_node(LINE, $2, $4, $1, $6, true); }
+        | tVOID tID '(' vars ')' body { $$ = new pwn::function_def_node(LINE, $2, $4, new basic_type(0,basic_type::TYPE_VOID), $6, true); }
+        | '*' tID '(' vars ')' body   { $$ = new pwn::function_def_node(LINE, $2, $4, new basic_type(4,basic_type::TYPE_POINTER), $6, true); }
+        | type tID '(' ')' body  { $$ = new pwn::function_def_node(LINE, $2, NULL, $1, $5, true); }
+        | tVOID tID '(' ')' body { $$ = new pwn::function_def_node(LINE, $2, NULL, new basic_type(0,basic_type::TYPE_VOID), $5, true); }
+        | '*' tID '(' ')' body   { $$ = new pwn::function_def_node(LINE, $2, NULL, new basic_type(4,basic_type::TYPE_POINTER), $5, true); }
+        ;
+
+f_call  : tID '(' ')'       { $$ = new pwn::function_call_node(LINE, $1, NULL); }
+        | tID '(' arg_call ')'  { $$ = new pwn::function_call_node(LINE, $1, $3); }
+        ;
+
+type    : tINT        { $$ = new basic_type(4, basic_type::TYPE_INT); }
+        | tSTRING     { $$ = new basic_type(4, basic_type::TYPE_STRING); }
+        ;
+
+arg_call: expr        { $$ = new cdk::sequence_node(LINE, $1); }
+        | arg_call ',' expr { $$ = new cdk::sequence_node(LINE, $3, $1); }
+        ;
+
+vars    : type tID           { $$ = new cdk::sequence_node(LINE, new pwn::var_node(LINE, $1, $2)); }
+        | vars ',' type tID  { $$ = new cdk::sequence_node(LINE, new pwn::var_node(LINE, $3, $4), $1); }
+        ;
+
+body    : '{' decs stmts '}' { $$ = new pwn::block_node(LINE, $2, $3); }
+        | '{' decs '}'       { $$ = new pwn::block_node(LINE, $2, NULL); }
+        | '{' stmts '}'      { $$ = new pwn::block_node(LINE, NULL, $2); }
+        | '{' '}'            { $$ = new pwn::block_node(LINE, NULL, NULL); }
+        ;
+
+stmts   : stmt           { $$ = new cdk::sequence_node(LINE, $1); }
+        | stmts stmt     { $$ = new cdk::sequence_node(LINE, $2, $1); }
+        ;
+
+stmt    : expr tEND           { $$ = new pwn::evaluation_node(LINE, $1); }
+        | expr '!'            { $$ = new pwn::print_node(LINE, $1); }
+        | tSTOP tEND          { $$ = new pwn::stop_node(LINE); }
+        | tSTOP tINTEGER tEND { $$ = new pwn::stop_node(LINE, $2); }
+        | tNEXT tEND          { $$ = new pwn::next_node(LINE); }
+        | tNEXT tINTEGER tEND { $$ = new pwn::next_node(LINE, $2); }
+        | body                { $$ = $1; }
+        | tIF '(' expr ')' stmt %prec tIFX            { $$ = new cdk::if_node(LINE, $3, $5); }
+        | tIF '(' expr ')' stmt tELSE stmt            { $$ = new cdk::if_else_node(LINE, $3,$5, $7); }
+        | tREPEAT '(' expr ',' expr ',' expr ')' stmt { $$ = new pwn::repeat_node(LINE, $3, $5, $7, $9); }
+        | tREPEAT '(' ',' expr ',' expr ')' stmt      { $$ = new pwn::repeat_node(LINE, NULL, $4, $6, $8); }
+        | tRETURN tEND                                { $$ = new pwn::return_node(LINE); }
+        ;
+
+expr    : '+' expr %prec tUNARY                           { $$ = new pwn::identity_node(LINE, $2); }
+        | '-' expr %prec tUNARY                           { $$ = new cdk::neg_node(LINE, $2); }
+        | '~' expr %prec tUNARY                           { $$ = new pwn::not_node(LINE, $2); }
+        | expr '+' expr                                   { $$ = new cdk::add_node(LINE, $1, $3); }
+        | expr '-' expr                                   { $$ = new cdk::sub_node(LINE, $1, $3); }
+        | expr '*' expr                                   { $$ = new cdk::mul_node(LINE, $1, $3); }
+        | expr '/' expr                                   { $$ = new cdk::div_node(LINE, $1, $3); }
+        | expr '%' expr                                   { $$ = new cdk::mod_node(LINE, $1, $3); }
+        | expr '<' expr                                   { $$ = new cdk::lt_node(LINE, $1, $3); }
+        | expr '>' expr                                   { $$ = new cdk::gt_node(LINE, $1, $3); }
+        | expr tGE expr                                   { $$ = new cdk::ge_node(LINE, $1, $3); }
+        | expr tLE expr                                   { $$ = new cdk::le_node(LINE, $1, $3); }
+        | expr tNE expr                                   { $$ = new cdk::ne_node(LINE, $1, $3); }
+        | expr tEQ expr                                   { $$ = new cdk::eq_node(LINE, $1, $3); }
+        | expr '|' expr                                   { $$ = new pwn::or_node(LINE, $1, $3); }
+        | expr '&' expr                                   { $$ = new pwn::and_node(LINE, $1, $3); }
+        | lval '?'                                        { $$ = new pwn::address_node(LINE, $1); }
+        | lval                                            { $$ = new pwn::rvalue_node(LINE, $1); }
+        | lval '=' expr                                   { $$ = new pwn::assignment_node(LINE, $1, $3); }
+        | '(' expr ')'                                    { $$ = $2; }
+        | literal                                         { $$ = $1; }
+        | f_call                                          { $$ = $1; }
+        ;
+
+literal : tINTEGER    { $$ = new cdk::integer_node(LINE, $1); }
+        | tSTRING     { $$ = new cdk::string_node(LINE, $1); }
+        | tNOOB       { $$ = new pwn::noob_node(LINE); }
+        ;
+
+lval    : tID          { $$ = new pwn::lvalue_node(LINE, $1); /* FIXME*/ }
+        | tID '[' expr ']' { $$ = new pwn::index_node(LINE, $1, $3); }
+        ;
+
+%%
